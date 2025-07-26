64a53225a95bf041b3d264b5709e868948ae1a66
towan <james.hammond@headforwards.com>
2025-07-26 13:01:58 +0100

    validation of CombinedWith function params

---
+++ api/redington.calculation.test/ProbabilityCalculatorTests.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+
+namespace redington.calculation.test;
+
+public class ProbabilityCalculatorTests
+{
+    //CombinedWith: P(A)P(B) e.g. 0.5 * 0.5 = 0.25
+    [Fact]
+    public void ValidateCombinedWith()
+    {
+        var result = ProbabilityCalculator.CombinedWith(0.5, 0.5);
+
+        result.Success.Should().BeTrue();
+        result.Result.Should().Be(0.25);
+    }
+
+
+    //The user should be able to enter two valid probabilities (0 to 1 e.g. 0.5 is valid, while 1.1 or -0.1 is not).
+    [Theory]
+    [InlineData(0.5, 0.5, true)]
+    [InlineData(1.1, 0.5, false)]
+    [InlineData(0.5, 1.1, false)]
+    [InlineData(-0.1, 0.5, false)]
+    [InlineData(0.5, -0.1, false)]
+    [InlineData(-0.1, -0.1, false)]
+    [InlineData(1.1, 1.1, false)]
+    public void ValidateParameters(double pa, double pb, bool expected)
+    {
+        var result = ProbabilityCalculator.CombinedWith(pa, pb);
+        result.Success.Should().Be(expected);
+    }
+}
---
+++ api/redington.calculation/ProbabilityCalculator.cs
@@ -0,0 +1,19 @@
+﻿
+namespace redington.calculation;
+
+
+
+/// <summary>
+/// ProbabilityCalculator 
+/// Calculations are 'pure' functions so can be defined as static
+/// </summary>
+public static class ProbabilityCalculator
+{
+    public static CalculationResult CombinedWith(double pa, double pb)
+    {
+        if (pa > 1.0 || pb > 1.0 || pa < 0 || pb < 0) return new CalculationResult(null, false);
+        var result = pa * pb;
+        return new CalculationResult(result,true);
+    }
+}
+
