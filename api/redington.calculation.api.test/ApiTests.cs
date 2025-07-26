af8e8a7a59a895297c991f0857da618d549054f7
towan <james.hammond@headforwards.com>
2025-07-26 14:51:28 +0100

    simplest Api test for CombinedWith endpoint

---
+++ api/redington.calculation.api.test/ApiTests.cs
@@ -0,0 +1,18 @@
+﻿namespace redington.calculation.api.test;
+
+using Alba;
+
+public class ApiTests
+{
+    [Fact]
+    public async Task CombinedWithEndpointReturnsOkStatus()
+    {
+        var host = await AlbaHost.For<global::Program>(x => {});
+        
+        await host.Scenario(_ =>
+        {
+            _.Get.Url("/CombinedWith?probabilityA=0.5&probabilityB=0.5");
+            _.StatusCodeShouldBeOk();
+        });
+    }
+}
--- api/redington.calculation.api/Program.cs
+++ api/redington.calculation.api/Program.cs
@@ -1,3 +1,5 @@
+using redington.calculation;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -14,28 +16,14 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
+app.MapGet("/CombinedWith", (double probabilityA, double probabilityB) =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+    var result = ProbabilityCalculator.CombinedWith(probabilityA, probabilityB);
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+    return Results.Ok(result);
+});
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+
+
