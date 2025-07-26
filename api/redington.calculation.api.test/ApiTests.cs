3e6997184a827cac8b83b969442f0e314db28356
towan <james.hammond@headforwards.com>
2025-07-26 15:09:51 +0100

    endpoints tested for ok and BadRequest scenarios

--- api/redington.calculation.api.test/ApiTests.cs
+++ api/redington.calculation.api.test/ApiTests.cs
@@ -1,18 +1,61 @@
 ﻿namespace redington.calculation.api.test;
 
 using Alba;
+using System.Net;
 
 public class ApiTests
 {
     [Fact]
     public async Task CombinedWithEndpointReturnsOkStatus()
     {
-        var host = await AlbaHost.For<global::Program>(x => {});
-        
+        var host = await AlbaHost.For<global::Program>(x => { });
+
         await host.Scenario(_ =>
         {
             _.Get.Url("/CombinedWith?probabilityA=0.5&probabilityB=0.5");
             _.StatusCodeShouldBeOk();
+            _.ContentShouldContain("0.25");
         });
+    }
+
+    [Fact]
+    public async Task CombinedWithEndpointWhenValidationFails()
+    {
+        var host = await AlbaHost.For<global::Program>(x => { });
+
+        await host.Scenario(_ =>
+        {
+            _.Get.Url("/CombinedWith?probabilityA=1.5&probabilityB=0.5");
+            _.StatusCodeShouldBe(HttpStatusCode.BadRequest);
+            _.ContentShouldContain("The value of pa must be between 0.0 and 1.0 inclusive.");
+        });
+
+    }
+    
+    [Fact]
+    public async Task EitherEndpointReturnsOkStatus()
+    {
+        var host = await AlbaHost.For<global::Program>(x => { });
+
+        await host.Scenario(_ =>
+        {
+            _.Get.Url("/Either?probabilityA=0.5&probabilityB=0.5");
+            _.StatusCodeShouldBeOk();
+            _.ContentShouldContain("0.75");
+        });
+    }
+
+    [Fact]
+    public async Task EitherEndpointWhenValidationFails()
+    {
+        var host = await AlbaHost.For<global::Program>(x => { });
+
+        await host.Scenario(_ =>
+        {
+            _.Get.Url("/Either?probabilityA=1.5&probabilityB=0.5");
+            _.StatusCodeShouldBe(HttpStatusCode.BadRequest);
+            _.ContentShouldContain("The value of pa must be between 0.0 and 1.0 inclusive.");
+        });
+ 
     }
 }
--- api/redington.calculation.api/Program.cs
+++ api/redington.calculation.api/Program.cs
@@ -1,3 +1,4 @@
+using System.Security.Cryptography;
 using redington.calculation;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -20,9 +21,25 @@ app.MapGet("/CombinedWith", (double probabilityA, double probabilityB) =>
 {
     var result = ProbabilityCalculator.CombinedWith(probabilityA, probabilityB);
 
+    if (!result.Success)
+    {
+        return Results.BadRequest(result.ValidationFailureMessage);    
+    }
+
     return Results.Ok(result);
 });
 
+app.MapGet("/Either", (double probabilityA, double probabilityB) =>
+{
+    var result = ProbabilityCalculator.Either(probabilityA, probabilityB);
+
+    if (!result.Success)
+    {
+        return Results.BadRequest(result.ValidationFailureMessage);    
+    }
+
+    return Results.Ok(result);
+});
 app.Run();
 
 
